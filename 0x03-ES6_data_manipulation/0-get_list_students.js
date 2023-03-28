8e6d6c80f0211b2d263642c4d62e69919494002a
hawkinswinja <hawkinswinja@gmail.com>
2023-03-28 18:10:20 +0300

    update firstname to firstName

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   const students = [];
-  students.push({ id: 1, firstname: 'Guillaume', location: 'San Francisco' });
-  students.push({ id: 2, firstname: 'James', location: 'Columbia' });
-  students.push({ id: 5, firstname: 'Serena', location: 'San Francisco' });
+  students.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  students.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  students.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
   return students;
 }
