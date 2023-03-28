400437d468dbf7420bb668afb2d1779a9bd07927
hawkinswinja <hawkinswinja@gmail.com>
2023-03-28 19:03:10 +0300

    implementation of task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
-
-  if (position >= length) {
+  try {
+    view[position] = value;
+  }
+  catch(err) {
     throw new Error('Position outside range');
   }
-  view[position] = value;
   return buffer;
 }
