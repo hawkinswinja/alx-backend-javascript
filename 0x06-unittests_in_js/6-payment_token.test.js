b8f0254b700878f10e7c38502ac4bff923654311
hawkinswinja <hawkinswinja@gmail.com>
2023-05-20 01:00:40 +0300

    update task 6 and 7

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,8 +4,9 @@ const assert = require('assert');
 describe('asynchronous call', function () {
   it('resolves a promise', function (done) {
     token.then((val) => {
-      assert.equal(val.get('data'), 'Successful response from the API');
+      done();
+    }).catch ((e) => {
+      done(e);
     });
-    done();
   });
 });
